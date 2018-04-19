d4b6f528ff3b432e45ced6e762bbc0c975603464
wallacer513 <rjw6cc@virginia.edu>
2018-04-18 22:35:01 -0400

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,18 @@
 <?php
+	class DbUtil{
+		public static $user = "CS4750asg8bz";
+		public static $pass = "spring2018";
+		public static $host = "stardock.cs.virginia.edu";
+		public static $schema = "CS4750asg8bz";
 
-$connect=mysqli_connect('stardock.cs.virginia.edu','CS4750asg8bz','spring2018','CS4750asg8bz');
- 
-if(mysqli_connect_errno($connect))
-{
-		echo 'Failed to connect';
-}
- 
-?>
+		public static function loginConnection() {
+			$db = new mysqli(DbUtil::$host, DbUtil::$user, DbUtil::$pass, DbUtil::$schema);
+			if($db->connect_errno) {
+				echo "fail";
+				$db->close();
+				exit();
+			}
+			return $db;
+		}
+	}
+?> 
