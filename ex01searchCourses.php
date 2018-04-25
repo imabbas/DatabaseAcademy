6ef1e1dec5f9a3d0662bb05d6377f25ed11844da
Katherine Yan <katherine.yan2015@gmail.com>
2018-04-24 23:12:19 -0400

    ajax

--- ex01searchCourses.php
+++ ex01searchCourses.php
@@ -6,12 +6,12 @@
 
         $stmt = $db->stmt_init();
 
-        if($stmt->prepare("select class_name from Class where class_name like ?") or die(mysqli_error($db))) {
+        if($stmt->prepare("select class_name, subject from Class where class_name like ?") or die(mysqli_error($db))) {
                 $searchString = '%' . $_GET['searchField'] . '%';
                 $stmt->bind_param("s", $searchString);
                 $stmt->execute();
                 $stmt->bind_result($class_name, $subject);
-                echo "<table border=1><th>Class Name</th><th>Subject></th>\n";
+                echo "<table border=1; width=450><th>Class Name</th><th>Subject</th>\n";
                 while($stmt->fetch()) {
                         echo "<tr><td>$class_name</td><td>$subject</td></tr>";
                 }
