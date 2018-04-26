ace85cc08b3d919b5e4fda63e7433766483753f1
Azman Singh Garcha <azgarcha@d-172-25-99-45.dhcp.virginia.edu>
2018-04-25 22:52:58 -0400

    commit message

--- gpacalc.php
+++ gpacalc.php
@@ -1,7 +1,7 @@
 <?php
 $fn = $_POST['f_name'];
 $ln = $_POST['l_name'];
-$g = (float)$_POST['grade'];
+$g = floatval($_POST['grade']);
 
 $con = new mysqli('stardock.cs.virginia.edu', 'CS4750asg8bz', 'spring2018', 'CS4750asg8bz');
 if (mysqli_connect_errno())
@@ -9,14 +9,17 @@ if (mysqli_connect_errno())
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$sql="CALL recalc_GPA($fn, $ln, $g)" or die('could not calculate');
-$result = $con -> prepare($sql) or die('could not prepare');
-$result->setFetchMode(PDO::FETCH_ASSOC) or die("could not fetch");
 
-while ($values = $result ->fetch())
+$sql="CALL recalc_GPA('$fn', '$ln', $g)";
+
+if (!mysqli_query($con,$sql))
 {
-  print "<pre>";
-  print_r($values);
+  die(header("location:addGrade.php?valid0=true&reason=failure"));
 }
+
+else{
+  header("location:addGrade.php?valid1=true&reason=success");
+}
+
 
 ?>
