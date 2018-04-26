923630f1f8cd2380ad8b56ba85123cd97ac7f136
imabbas <aadil.abbas96@gmail.com>
2018-04-25 20:31:14 -0400

    click on any user

--- personDetail.php
+++ personDetail.php
@@ -4,21 +4,44 @@
 
         $stmt = $db->stmt_init();
 
-        if($stmt->prepare("(select f_name, l_name, salary, email from Teachers where f_name like ?) UNION (select f_name, l_name from Students where f_name like ?)") or die(mysqli_error($db))) {
+        if($stmt->prepare("(select f_name, l_name, email, salary from Teachers where f_name like ?)") or die(mysqli_error($db))) {
                 $searchString = '%' . $_POST['first_name'] . '%';
 
                 $stmt->bind_param("s", $searchString);
                 $stmt->execute();
-                $stmt->bind_result($f_name, $l_name, $salary, $email);
-                echo "</br>";
-                echo "Results";
-                echo "<table border=1; width=450><th>Name</th><th>Salary</th><th>Email</th>\n";
-                while($stmt->fetch()) {
-                        echo "<tr><td class='item'>$f_name $l_name</td><td> $salary</td><td> $email</td></tr>";
-                }
-                echo "</table>";
+                $stmt->store_result();
+                if($stmt->num_rows > 0) {
+                  $stmt->bind_result($f_name, $l_name, $email, $salary);
+                  echo "</br>";
+                  echo "Results";
+                  echo "<table border=1; width=450><th>Name</th><th>Email</th><th>Salary</th>\n";
+                  while($stmt->fetch()) {
+                          echo "<tr><td class='item'>$f_name $l_name</td><td>$email</td><td>$salary</td></tr>";
+                  }
+                  echo "</table>";
+                  $stmt->close();
+                } else{
+                    $meme = $db->stmt_init();
+                    if($meme->prepare("(select f_name, l_name, email, gpa, classes_taken from Students where f_name like ?)") or die(mysqli_error($db))) {
+                            $searchString = '%' . $_POST['first_name'] . '%';
 
-                $stmt->close();
+                            $meme->bind_param("s", $searchString);
+                            $meme->execute();
+                            $meme->store_result();
+                            if($meme->num_rows > 0) {
+                              $meme->bind_result($f_name, $l_name, $email, $gpa, $classes);
+                              $string = strval($gpa);
+                              echo "</br>";
+                              echo "Results";
+                              echo "<table border=1; width=450><th>Name</th><th>Email</th><th>GPA</th><th>Classes Taken</th>\n";
+                              while($meme->fetch()) {
+                                      echo "<tr><td class='item'>$f_name $l_name</td><td>$email</td><td>$string</td><td>$classes</td></tr>";
+                              }
+                              echo "</table>";
+                              $meme->close();
+                            }
+                    }
+                  }
         }
 
         $db->close();
